985f557e802765656bb7c879900bf0a4c368d898
maykol68 <sm9365277@gmail.com>
2026-01-07 22:03:20 -0500

    View Logo Helper Mehtod

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def logo(size: 'text-xl', extra_classes: '')
+    link_to root_path, class: "flex items-center font-bold text-gray-900 #{size} #{extra_classes}" do
+      safe_join([
+        content_tag(:i, '', class: 'bi bi-safe-fill text-indigo-600 mr-2'),
+        content_tag(:span, 'SafePass')
+      ])
+    end
+  end
 end
