882ce35f51139b347b56f5ad49f139ffc28c44e9
maykol68 <sm9365277@gmail.com>
2026-01-15 21:34:12 -0500

    Controller - Create action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,26 @@
 class EntriesController < ApplicationController
+    before_action :authenticate_user!
+
     def new
         @entry = Entry.new 
+    end
+
+    def create
+        @entry = current_user.entries.new(entry_params)
+
+        if @entry.save
+            flash[:notice] = "Entry has been created!"
+            redirect_to root_path
+        else
+            flash[:alert] = "Sorry, there was an issue!"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def entry_params
+        # params.require(:entry).permit(:name, :url, :username, :password)
+        params.expect(entry: [:name, :url, :username, :password])
     end
 end
