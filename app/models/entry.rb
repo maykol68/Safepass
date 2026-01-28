708fec55112cffbf149e6aea6f355087b3e18b6f
maykol68 <sm9365277@gmail.com>
2026-01-27 19:35:25 -0500

    Reproducir 19. Model - Entry Database Validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,14 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  validates :name, :username, :password, presence: true
+  validates :url_must_be_valid
+
+  private
+
+  def url_must_be_valid
+    unless url.include?('http' || 'https')
+      errors.add(:url, ' must be valid')
+    end
+  end
 end
